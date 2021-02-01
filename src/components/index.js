2f9920f5055cf708d52e2a58c69f642507ad55d9
jamieeunice <jamieecarrasquillo@gmail.com>
2021-01-31 21:52:53 -0600

    feat(user-position)  add user position tracking ability

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+// this `index.js` is a great place to export everything
+// when searching, the first file its found is always index.js
+// so this allows every component to be found here
+// rather than having to go to each component file separately
+
+export { Game } from "./Game";
+export { ChatLobby } from "./ChatLobby";
+export { Home } from "./Home";
+export { Space } from "./Space";
